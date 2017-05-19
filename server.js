7628e8122503f5ffad8a973c2fa17d4bc582d293
Sharon Zavala <sharon@MacBook-Pro.local>
2017-05-18 19:55:59 -0700

    ugh dunno

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 var express = require("express");
+var http = require("http");
 var bodyParser = require("body-parser");
 var path = require("path");
-
+var fs = require("fs");
 
 var app = express();
 
+var port = 3000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.text());
-app.use(express.static('public'))
+app.use(express.static('public'));	
 
-app.get("/", function(req, res){
-	res.sendFile(path.join(__dirname, "home.html"));
-});
 
-app.get("/survey", function(req, res){
-	res.sendFile(path.join(__dirname, "survey.html"));
-});
 
 /*
 app.post("/new", function(req, res){
@@ -23,7 +20,9 @@ app.post("/new", function(req, res){
 });
 */
 
-app.listen(3000);
+app.listen(3000, function (){
+	console.log("listening on port: " + 3000);
+});
 
 
 
